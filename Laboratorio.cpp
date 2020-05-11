e0e0f7a5a12fc9c683204b0dea59e3888e8769f0
Brankike <00268417@uca.edu.sv>
2020-05-11 00:44:47 -0600

    Laboratorio 11 de Mayo

--- Laboratorio.cpp
+++ Laboratorio.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-using namespace std;
+using namespace std; /*con plecas se deja la comentarios*/
 int main()
 {
     cout << "Hola Mundo" << endl;
 }
-/*con plecas se deja la comentarios*/
